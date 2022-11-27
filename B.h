5eb874ba52d82464a9ef12146adf564a88636b27
nohelia <ntacca@unsa.edu.pe>
2022-11-27 17:03:50 -0500

    creamos la clase B.h y C.h abstractas

---
+++ B.h
@@ -0,0 +1,9 @@
+#ifndef B_H
+#define B_H
+
+class B{
+  pulic:
+    virtual void bbb()=0;
+}
+
+#endif
---
+++ C.h
@@ -0,0 +1,9 @@
+#ifndef C_H
+#define C_H
+
+class C{
+  pulic:
+    virtual void ccc()=0;
+}
+
+#endif
