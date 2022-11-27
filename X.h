691780ed64557e7b07a15900d6cc36fadb052f7a
nohelia <ntacca@unsa.edu.pe>
2022-11-27 17:11:33 -0500

    se crearon las clases X y Y

---
+++ X.h
@@ -0,0 +1,8 @@
+#ifndef X_H
+#define X_H
+class X {
+  public:
+    virtual B foo(B)=0;
+};
+
+#endif
---
+++ Y.h
@@ -0,0 +1,8 @@
+#ifndef Y_H
+#define Y_H
+class Y:public X {
+  public:
+    B foo(A);
+};
+
+#endif
